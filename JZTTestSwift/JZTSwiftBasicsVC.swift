c002a2d0164a7c228803bebe18a27f4ee7ae4f2d
赵奎 <zhaokui@ehaoyao.com>
2019-10-15 11:56:16 +0800

    test

---
+++ JZTTestSwift/JZTSwiftBasicsVC.swift
@@ -0,0 +1,19 @@
+//
+//  JZTSwiftBasicsVC.swift
+//  JZTTestSwift
+//
+//  Created by 赵奎 on 2019/10/15.
+//  Copyright © 2019 赵奎. All rights reserved.
+//
+
+import UIKit
+
+class JZTSwiftBasicsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let testNumber = 10
+        print("test ----\(testNumber)")
+    }
+
+}
