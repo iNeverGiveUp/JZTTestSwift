cabc21bb108ff58a6adc9ae44e6a2731ee5815fd
赵奎 <zhaokui@ehaoyao.com>
2019-10-15 10:54:17 +0800

    first

---
+++ JZTTestSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZTTestSwift
+//
+//  Created by 赵奎 on 2019/10/15.
+//  Copyright © 2019 赵奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JZTTestSwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JZTTestSwift
+//
+//  Created by 赵奎 on 2019/10/15.
+//  Copyright © 2019 赵奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
